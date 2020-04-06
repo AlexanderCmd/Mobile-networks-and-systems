34ec2105aa61111ad26542f70c0c62b356b9e809
AlexanderCmd <36704713+AlexanderCmd@users.noreply.github.com>
2020-04-06 20:37:11 +0300

    Update hardHammingDecoder.m

--- task_2/hardHammingDecoder.m
+++ task_2/hardHammingDecoder.m
@@ -1,10 +1,9 @@
 function mX = hardHammingDecoder(matrixHammingDec, mH)
 
 S = mod(mH * matrixHammingDec, 2);
-if sum(S) == 0
-    mX = mH(1:1:end - 5);
-else
+if sum(S) ~= 0
     indexError = bi2de(S);
     mH(indexError) = xor(mH(indexError), 1);
-    mX = mH(1:1:end - 5);
 end
+
+mX = mH(1:1:end - 5);
